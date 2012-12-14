71b7832df5b048a58519bb1ea1e9bf3290fe1664
Andreas Tjärnberg <andreas.tjarnberg@fripost.org>
2012-12-15 00:22:39 +0100

    Cleaning duplicates from init.el

--- init.el
+++ init.el
@@ -16,8 +16,6 @@
 (blink-cursor-mode t)
 (set-face-attribute 'default nil :height 110) ;; This makes it so that emacs scale pixels correct. It doesn't in v 24 right now.
 (setq-default fill-column 70) ;; make newline insertion in the right place
-; (setq truncate-partial-width-windows nil) ;; make it so that several windows behave like the single window text wrapping. Seems default
-(auto-compression-mode t) ;; Make it so that emacs can open compressed files like .gz
 (column-number-mode 1) ;; Show column-number in the mode line
 (scroll-bar-mode -1) ;; Removes the scroll bar
 (tool-bar-mode -1) ;; Hide toolbar in graphics mode
@@ -25,9 +23,9 @@
 (global-set-key [f1] 'menu-bar-mode) ;; Toggle menu bar mode with f1
 (global-set-key [f12] 'toggle-truncate-lines) ;; toggle-truncate-lines keybinding
 (setq blink-matching-delay .1) ;; matching parenthesis timer default 1
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on) ;; Make colors work in M-x shell
 
 ;; load up emacs init
+(message "Loading emacs-init")
 (org-babel-load-file (expand-file-name "emacs-init.org" emacs-init-dir))
 
 (message "init.el has loaded")
