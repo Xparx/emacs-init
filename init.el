8956ac5fdb6ff053f716487014503a9850a3b675
Andreas Tjärnberg <andreas.tjarnberg@fripost.org>
2013-10-07 11:09:30 +0200

    Merge branch 'master' of github.com:Xparx/emacs-init

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-;;; init.el --- Where all the magic begins
-;;
 ;; This is the first thing to get loaded.
 ;;
 (require 'cl) ; a rare necessary use of REQUIRE, this is for common lisp!
@@ -15,28 +13,39 @@
 (setq inhibit-startup-screen t)
 (blink-cursor-mode t)
 (set-face-attribute 'default nil :height 110) ;; This makes it so that emacs scale pixels correct. It doesn't in v 24 right now.
-(setq-default fill-column 70) ;; make newline insertion in the right place
-(column-number-mode 1) ;; Show column-number in the mode line
-(scroll-bar-mode -1) ;; Removes the scroll bar
-(tool-bar-mode -1) ;; Hide toolbar in graphics mode
-(menu-bar-mode -1) ;; Remove the menu bar, Remove this when menu becomes part of the global menu.
-(global-set-key [f1] 'menu-bar-mode) ;; Toggle menu bar mode with f1
-(global-set-key [f12] 'toggle-truncate-lines) ;; toggle-truncate-lines keybinding
-(global-set-key [f11] 'visual-line-mode)      ;; toggle nice soft line wrapping
-(setq blink-matching-delay .1) ;; matching parenthesis timer default 1
-(setq-default indent-tabs-mode nil) ;; make sure no tabs are present.
+(setq-default fill-column 70)                 ;; Make newline insertion in the right place
+(column-number-mode 1)                        ;; Show column-number in the mode line
+(scroll-bar-mode -1)                          ;; Removes the scroll bar
+(tool-bar-mode -1)                            ;; Hide toolbar in graphics mode
+(menu-bar-mode -1)                            ;; Remove the menu bar, Remove this when menu becomes part of the global menu.
+(global-set-key [f1] 'menu-bar-mode)          ;; Toggle menu bar mode with f1
+(global-set-key [f12] 'toggle-truncate-lines) ;; Toggle-truncate-lines keybinding
+(global-set-key [f11] 'visual-line-mode)      ;; Toggle nice soft line wrapping
+(setq blink-matching-delay .1)                ;; Matching parenthesis timer default 1
+(setq-default indent-tabs-mode nil)           ;; Make sure no tabs are present.
 (setq-default tab-width 4)
 
+;; load el-get here.
+(setq el-get-dir (expand-file-name "el-get" emacs-init-dir))
+(add-to-list 'load-path (concat emacs-init-dir "el-get/el-get"))
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(add-to-list 'el-get-recipe-path (concat emacs-init-dir "recipes"))
+(el-get 'sync)
 
 ;; load up emacs init
 (message "Loading emacs-init")
-(require 'org)
-(org-babel-load-file (expand-file-name "emacs-init.org" emacs-init-dir))
 
-(message "init.el has loaded")
+(if (el-get-package-exists-p "org-mode")
+    (org-babel-load-file (expand-file-name "emacs-init.org" emacs-init-dir)))
 
-;; This didn't work with the version of emacs at work. may change later.
-;; (message "My init.el loaded in %ds" (destructuring-bind (hi lo ms) (current-time)
-;; 			     (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
+(message "init.el has loaded in %ds" (destructuring-bind (hi lo ms) (current-time) (- (+ hi lo) (+ (first *emacs-load-start*) (second *emacs-load-start*)))))
 
 ;;; init.el ends here
