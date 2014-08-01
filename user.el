d44615575216ba4018d3ea09cd0f076f4b8bdc20
Andreas Tjärnberg <andreas.tjarnberg@fripost.org>
2014-08-02 01:25:56 +0200

    Example file for user information
    Avoid committing this file with:
    $ git update-index --assume-unchanged user.el

---
+++ user.el
@@ -0,0 +1,5 @@
+;; ;; Example code
+;; ;; Ignore changes in this file with
+;; ;; $ git update-index --assume-unchanged user.el
+;; (setq user-full-name "User Name"
+;;       user-mail-address "user@email.address)
